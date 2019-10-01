c73b8835dccafbba59d4a71e67cb994f72a7ff33
Victor Pierre <victor.pierre@student.ecp.fr>
2019-10-01 15:28:09 +0200

    point initializer updated

--- models/point.py
+++ models/point.py
@@ -9,13 +9,15 @@ import os
 
 load_dotenv()
 
-
-
 class Point:
-    def __init__(self, lat, long):
-        self.lat=lat
-        self.long=long
-        #self.address = API(lat, long)
+    def __init__(self, lat=None, long=None, address=None):
+        if address is None:
+            self.lat = lat
+            self.long = long
+            self.address = self.get_adr_from_cor()
+        else:
+            self.address = address
+            self.lat, self.long = self.get_cor_from_adr()
 
     def to_LatLong(self):
         return str(self.lat)+','+str(self.long)
@@ -24,25 +26,25 @@ class Point:
         return str(self.long)+','+str(self.lat)
 
     def get_adr_from_cor(self):
-            api_key = os.getenv("LOCATION_IQ_KEY")
-            latitude = self.lat
-            longitude = self.long
-            data = dict(key=api_key, lat=str(latitude), lon=str(longitude), format='json')
+        api_key = os.getenv("LOCATION_IQ_KEY")
+        latitude = self.lat
+        longitude = self.long
+        data = dict(key=api_key, lat=str(latitude), lon=str(longitude), format='json')
 
-            reponse = requests.get("https://us1.locationiq.com/v1/reverse.php", params=data)
-            resp = reponse.json()
-            return resp['display_name']
+        reponse = requests.get("https://us1.locationiq.com/v1/reverse.php", params=data)
+        resp = reponse.json()
+        return resp['display_name']
 
-def get_cor_from_adr(adresse):
-    api_key = os.getenv("LOCATION_IQ_KEY")
-    data = dict(key=api_key, q=adresse, format='json')
-    reponse = requests.get("https://us1.locationiq.com/v1/search.php", params=data)
-    resp = reponse.json()
-    return (resp[0]['lat'], resp[0]['lon'])
+    def get_cor_from_adr(self):
+        api_key = os.getenv("LOCATION_IQ_KEY")
+        data = dict(key=api_key, q=self.address, format='json')
+        reponse = requests.get("https://us1.locationiq.com/v1/search.php", params=data)
+        resp = reponse.json()
+        return (resp[0]['lat'], resp[0]['lon'])
 
 if __name__ == "__main__":
     PointA = Point(48.693259, 2.155216)
     print(PointA.get_adr_from_cor())
-    print(get_cor_from_adr('Tour Eiffel'))
+    print(Point(address='Tour Eiffel'))
 
     ### TO DO : definir un point à partir d'une addresse
